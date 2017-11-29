d24eed44b7193fdbcb6a920f7eadcb8319931f02
jffernan <jffernan@comcast.net>
2017-11-28 18:48:52 -0800

    added styling to footer

--- src/components/OutputDisplay.js
+++ src/components/OutputDisplay.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
 const OutputDisplay = (props) =>
-  <div className="outputDisplay">
-    <br/>
-    <p className="temp-wrapper">Current Temperature:
-      <span className="temp">{ props.tempOutput }</span>
-      <span className="temp-symbol"> °F</span>
-    </p>
-    <p className="cond-wrapper">Current Conditions:
-      <span className="cond">{ props.condOutput }</span>
-    </p>
-    <br/>
+  <div>
+    <div className="outputDisplay">
+      <br/>
+      <p className="temp-wrapper">Current Temperature:
+        <span className="temp">{ props.tempOutput }</span>
+        <span className="temp-symbol"> °F</span>
+      </p>
+      <p className="cond-wrapper">Current Conditions:
+        <span className="cond">{ props.condOutput }</span>
+      </p>
+      <br/>
+    </div>
+    <footer className="App-footer">
+      <h4 className="App-code">
+        <a target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/jffernan/my-react-weather-app">
+          Visit GitHub To View My Code
+        </a>
+      </h4>
+    </footer>
   </div>
 
 export default OutputDisplay;
